ce30fa684e4b8fc640f8e360406a907713a1f410
Jeffrey Barg <jeffbarg94@gmail.com>
2018-01-18 00:43:11 -0500

    :whale2: Made network connection modal more descriptive

--- src/components/ConnectNetworkModal.jsx
+++ src/components/ConnectNetworkModal.jsx
@@ -11,7 +11,7 @@ import { connect } from 'react-redux'
 
 import { joinNetwork } from '../actions'
 
-import { Modal, Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap'
+import { Row, Col, Modal, Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap'
 
 class ConnectNetworkModal extends Component {
   constructor (props) {
@@ -41,19 +41,32 @@ class ConnectNetworkModal extends Component {
 
         <form onSubmit={(e) => { e.preventDefault() }}>
           <Modal.Body>
+            <Button
+              block
+              bsStyle='primary'
+              onClick={() => {
+                this.props.connectNode('littlecoin.network')
+                this.props.onHide()
+              }}>Join Main Network</Button>
+            <Row>
+              <Col xs={5}><hr /></Col>
+              <Col xs={2} style={{marginTop: 10, textAlign: 'center'}}><small>OR</small></Col>
+              <Col xs={5}><hr /></Col>
+            </Row>
+
             <FormGroup
               controlId='formBasicText'
               >
-              <ControlLabel>Network Name to Connect To</ControlLabel>
+              <ControlLabel>Choose Custom Network Name to Connect To</ControlLabel>
               <FormControl
                 type='text'
                 value={this.state.networkName}
                 placeholder='Network Name'
                 onChange={this.handlePeerNameChange}
-                autoFocus
                 />
               <FormControl.Feedback />
-              <HelpBlock>Copy the Network ID that you want to join on the <code>LittleCoin</code> protocol. (must be all lowercase letters, '.', or '-')</HelpBlock>
+              <HelpBlock>Copy the Network ID that you want to join on the <code>LittleCoin</code> protocol.</HelpBlock>
+              <HelpBlock>A "network" is simply an identifier that helps facilitate finding other peers who have joined the same network.  In a real-world public blockchain, you might start with a list of trusted peers' IP addresses and ask for their peers.  You would continue this process until you are convinced you have all (or enough) existing peers.</HelpBlock>
             </FormGroup>
           </Modal.Body>
 
@@ -62,7 +75,7 @@ class ConnectNetworkModal extends Component {
             <Button bsStyle='primary'
               type='submit'
               onClick={() => {
-                this.props.connectNode(this.state.networkName)
+                this.props.connectNode(this.state.networkName.toLowerCase())
                 this.props.onHide()
               }}
                   >Connect</Button>
